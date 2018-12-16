d48210beeb3aad6130d9429aa72f7487f7d338cb
Anzhelika Kirichuk <anzhelika.kirichuk@gmail.com>
2018-12-16 21:00:57 +0200

    debugging p11

---
+++ _nuxt/23811844171fb30a164c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{206:function(e,n,t){"use strict";t.r(n);var i={head:function(){return{title:"Playbook"}}},l=t(5),o=Object(l.a)(i,function(){var e=this.$createElement;return(this._self._c||e)("div")},[],!1,null,null,null);o.options.__file="index.vue";n.default=o.exports}}]);
